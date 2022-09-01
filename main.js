bd121ac6386f4c840b71c1d28ff6f1a8abc565a8
TanushAnand-whitehat <83594845+TanushAnand-whitehat@users.noreply.github.com>
2022-09-01 17:34:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 sound = "" ;
 song = "";
+
+rightWristx = 0;
+rightWristy = 0;
+leftWristx = 0;
+leftWristy = 0;
+
+function setup() {
+    canvas = createCanvas(700, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model has loaded");
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        rightWristx = results[0].pose.rightWrist.x;
+        rightWristy = results[0].pose.rightWrist.y;
+        leftWristx = results[0].pose.leftWrist.x;
+        leftWristy = results[0].pose.leftWrist.y;
+    }   
+}
 function preload() {
     sound = loadSound("music.mp3");
     song = loadSound("music2.mp3");
